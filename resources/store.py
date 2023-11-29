225f4abed734f0227e700449b360b8ef580a9040
edgar.afonso <edgar.afonso@fdmgroup.com>
2023-11-29 15:59:23 +0000

    Refactored POSTing logic to actually send the data to the database.

--- resources/store.py
+++ resources/store.py
@@ -2,6 +2,9 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from model import StoreModel
+from db import db
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 
 from schemas import StoreSchema
 
@@ -33,11 +36,18 @@ class StoreList(MethodView):
     @blp.arguments(StoreSchema)
     @blp.response(200, StoreSchema)
     def post(self, store_data):
-        for store in stores.values():
-            if store_data["name"] == store["name"]:
-                abort(400, message="Store already exists")
-
-        store_id = uuid.uuid4().hex
-        store = {**store_data, "id": store_id}
-        stores[store_id] = store
-        return store, 201
+        store = StoreModel(**store_data)
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
+            abort(
+                400,
+                message="A store with that name already exists"
+            )
+        except SQLAlchemyError:
+            abort(
+                500,
+                message="An error occured creating the store."
+            )
+        return store
